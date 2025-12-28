5742db169425a6ec1062f5136a135e7ce4474117
johanrussouw <jrussouw610505@gmail.com>
2025-12-28 15:11:55 +0200

    thrid bento element

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -161,7 +161,34 @@ const Page = () => {
                   />
                 </div>
               </div>
-              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]"></div>
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]" />
+              {/* third bento grid element */}
+              <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
+                <div className="absolute inset-px rounded-lg bg-white" />
+                <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
+                  <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                    <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
+                      Track Any Properties
+                    </p>
+                    <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                      Add any custom data you like to an event, such as a user
+                      email, a purchase amount or an exceeded quota.
+                    </p>
+                  </div>
+
+                  <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
+                    <Image
+                      className="w-full max-lg:max-w-xs"
+                      src="/bento-custom-data.png"
+                      alt="Bento box illustrating custom data tracking"
+                      width={500}
+                      height={300}
+                    />
+                  </div>
+                </div>
+
+                <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5" />
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
