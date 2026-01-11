f731d6ab6bfff4ea51c00fae7c167a348aeeb13c
johanrussouw <jrussouw610505@gmail.com>
2026-01-11 18:56:28 +0200

    events routes finished editing.

--- src/app/api/v1/events/route.ts
+++ src/app/api/v1/events/route.ts
@@ -98,7 +98,109 @@ export const POST = async (req: NextRequest) => {
 
   const dmChannel = await discord.createDM(user.discordId);
 
-  await discord.sendEmbed(dmChannel.id, {
-    title: "Hello world!!!",
+  let requestData: unknown;
+
+  try {
+    requestData = await req.json();
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (err) {
+    return NextResponse.json(
+      {
+        message: "Invalid JSON request body",
+      },
+      { status: 400 }
+    );
+  }
+
+  const validationResult = REQUEST_VALIDATOR.parse(requestData);
+
+  const category = user.EventCategories.find(
+    (cat) => cat.name === validationResult.category
+  );
+
+  if (!category) {
+    return NextResponse.json(
+      {
+        message: `You don not have a category named "${validationResult.category}"`,
+      },
+      { status: 404 }
+    );
+  }
+
+  const eventData = {
+    title: `${category.emoji || "🔔"} ${
+      category.name.charAt(0).toUpperCase() + category.name.slice(1)
+    }`,
+    description:
+      validationResult.description ||
+      `A new ${category.name} event has occurred!`,
+    color: category.color,
+    timestamp: new Date().toISOString(),
+    fields: Object.entries(validationResult.fields || {}).map(
+      ([key, value]) => {
+        return {
+          name: key,
+          value: String(value),
+          inline: true,
+        };
+      }
+    ),
+  };
+
+  const event = await db.event.create({
+    data: {
+      name: category.name,
+      formattedMessage: `${eventData.title}\n\n${eventData.description}`,
+      userId: user.id,
+      fields: validationResult.fields || {},
+      eventCategoryId: category.id,
+    },
+  });
+
+  try {
+    await discord.sendEmbed(dmChannel.id, eventData);
+
+    await db.event.update({
+      where: { id: event.id },
+      data: { deliveryStatus: "DELIVERED" },
+    });
+
+    await db.quota.upsert({
+      where: {
+        userId_year_month: {
+          userId: user.id,
+          year: currentYear,
+          month: currentMonth,
+        },
+      },
+      update: { count: { increment: 1 } },
+      create: {
+        userId: user.id,
+        month: currentMonth,
+        year: currentYear,
+        count: 1,
+      },
+    });
+  } catch (err) {
+    await db.event.update({
+      where: { id: event.id },
+      data: { deliveryStatus: "FAILED" },
+    });
+
+    console.log(err);
+
+    return NextResponse.json(
+      {
+        message: "Error processing event",
+        eventId: event.id,
+      },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json({
+    message: "Event processed successfully",
+    eventId: event.id,
   });
 };
